2a1ea3ae5bae86dd87bfb940ce59b4615f1f4c32
Khalfan AlKindi <khalfan500@hotmail.com>
2025-05-01 23:54:51 +0400

    Add productionupdate  domain to CSRF_TRUSTED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -202,10 +202,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",  # ✅ Alternative Localhost
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
